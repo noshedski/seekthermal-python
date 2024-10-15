380a10ccba158cac6f33e99af72e65eeea112383
antoine2304 <antoinelavigne04@gmail.com>
2024-10-15 14:55:44 -0400

    .

---
+++ examples/download.bash
@@ -0,0 +1,39 @@
+# Bash script to download images from pi and create video
+# Assume this script is run at examples directory
+
+# Pi address should be 192.168.2.2
+# ssh config file with name epi
+
+# Check if argument is provided
+if [ -z "$1" ]; then
+    echo "No video name provided. Aborting."
+    exit 1
+fi
+
+# Download images
+echo "Downloading images from pi"
+scp -r epi:/home/pi/seekthermal-python/examples/frames .
+
+# Delete last frame
+# echo "Deleting last frame"
+# last_frame=$(ls examples/frames | sort -n | tail -1)
+# echo "Last frame: $last_frame"
+# rm "examples/frames/$last_frame"
+
+# Create video
+echo "Creating video"
+/usr/bin/env python3 merge_imagefiles.py $1
+
+echo "Video created"
+
+# Delete images from pi
+echo "Deleting images from pi"
+ssh epi "rm /home/pi/seekthermal-python/examples/frames/*"
+
+# Delete images
+echo "Deleting images"
+rm frames/*
+
+# Open video
+echo "Opening video"
+open videos/$1.mp4
