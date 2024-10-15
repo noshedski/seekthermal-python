eb4155e9250f3f2e929f3c9fb157472b20d365cf
antoine2304 <antoinelavigne04@gmail.com>
2024-10-15 15:01:47 -0400

    .

---
+++ examples/monitor_message.py
@@ -0,0 +1,49 @@
+import asyncio
+from mavsdk import System
+import os
+from seekcamera_opencv_record import main
+
+"""
+Message interface
+
+Incomming
+I/start-video:{seconds}
+
+Outgoing
+F/video-started
+F/video-stopped
+
+"""
+
+async def run():
+
+    print("Initializing system...")
+    drone = System(sysid=1)
+    system_address = os.getenv("MAV_DEV", "udp://:14540")
+    print(f"Connecting to drone on: {system_address}")
+    await drone.connect(system_address=system_address)
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Connected")
+            break
+
+    async for status_message in drone.telemetry.status_text():
+        print(f"STATUS_MESSAGE: {status_message.text}")
+        if (status_message.text.includes("C/start-video")):
+            print("Start video")
+            # Start video recording
+            try:
+                _, command = status_message.text.split(":")
+                seconds = int(command.split("-")[1])
+                print(f"Starting video for {seconds} seconds")
+                # Add your video recording logic here
+
+            except (ValueError, IndexError) as e:
+                print(f"Error parsing command: {e}")
+            break
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
