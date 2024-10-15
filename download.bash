e308faaeca762f96d5b37d89044a75df5854c405
antoine2304 <antoinelavigne04@gmail.com>
2024-10-15 14:14:19 -0400

    Update bash file

--- download.bash
+++ download.bash
@@ -2,18 +2,27 @@
 # Assume this script is run at the root of seekthermal directory
 
 # Pi address should be 192.168.2.2
+# ssh config file with name epi
 
 # Download images
-# TODO: Complete the path on the pi
 echo "Downloading images from pi"
-scp -r pi@192.68.2.2:/ examples/videos
+scp -r epi:/home/pi/seekthermal-python/examples/frames examples/frames
 
-# TODO delete last frame
+# Delete images from pi
+echo "Deleting images from pi"
+ssh epi "rm -r /home/pi/seekthermal-python/examples/frames"
+
+# Delete last frame
+echo "Deleting last frame"
+last_frame=$(ls examples/frames | sort -n | tail -1)
+rm "examples/frames/$last_frame"
 
 # Create video
-# TODO complete the path
 echo "Creating video"
-python examples/merge_images.py examples/videos 
+python examples/merge_images.py $1
 
-# TODO add path to the eco message
 echo "Video created"
+
+# Open video
+echo "Opening video"
+open examples/video/$1.mp4
