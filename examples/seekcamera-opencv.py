b1308a52ada75810305bf7fd33310e428c0be170
noah <noahhturpinn@gmail.com>
2024-11-26 15:20:10 -0500

    .

--- examples/seekcamera-opencv.py
+++ examples/seekcamera-opencv.py
@@ -155,7 +155,7 @@ def main():
                         #print(height)
                         #print(width)
                         #print(test)
-                        cv2.resizeWindow(window_name, 640, 440)
+                        cv2.resizeWindow(window_name, 640, 480)
                         renderer.first_frame = False
 
                     # Render the image to the window.
